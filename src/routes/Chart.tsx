9eb7ad71bb2aec4cc9108d02a4e8e29019860fa9
YH Kim <coral272@naver.com>
2023-04-10 12:01:21 +0900

    Line chart => Candlestick chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -31,50 +31,37 @@ function Chart({ coinId }: ChartProps) {
         "Loading chart..."
       ) : (
         <ApexChart
-          type="line"
+          type="candlestick"
           series={[
             {
-              name: "price",
-              data: data?.map((price) => parseFloat(price.close)) ?? [],
-              // data: data?.map((price) => parseFloat(price.close)) ?? [],
-              // ??(null병합 연산자)는 왼쪽 피연산자가 null || undefined 일때 오른쪽 피연산자를 반환
-              // ApexChart에 들어가는 data는 number여야 하는데, data를 못받아왔을 때(undefined)의 경우(빈 배열을 반환)도 처리하는 것임
+              data:
+                data?.map((price) => ({
+                  x: price.time_close * 1000,
+                  y: [
+                    parseFloat(price.open),
+                    parseFloat(price.high),
+                    parseFloat(price.low),
+                    parseFloat(price.close),
+                  ],
+                })) ?? [],
             },
           ]}
           options={{
             theme: {
               mode: "dark",
             },
-            stroke: {
-              curve: "smooth",
-            },
-            grid: {
-              show: true,
-            },
-            yaxis: {
-              show: false,
-            },
-            xaxis: {
-              labels: { show: false },
-              axisBorder: { show: false },
-              axisTicks: { show: false },
-              type: "datetime",
-              categories: data?.map((price) => price.time_close * 1000),
-            },
-            fill: {
-              type: "gradient",
-              gradient: { gradientToColors: ["#0fbcf9"] },
-            },
-            colors: ["#0be881"],
-            tooltip: {
-              y: {
-                formatter: (value) => `$${value.toFixed(2)}`,
-              },
-            },
             chart: {
-              toolbar: { show: false },
               height: 500,
               width: 500,
+              toolbar: {
+                show: false,
+              },
+            },
+            yaxis: {
+              show: true,
+            },
+            xaxis: {
+              type: "datetime",
             },
           }}
         />
